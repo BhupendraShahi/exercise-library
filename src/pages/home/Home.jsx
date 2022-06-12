2c168084371449c595d3c52025685861acc2fb22
Bhupendra <shahi23.bhupendra11@gmail.com>
2022-06-12 11:50:46 +0530

    add mailList component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,6 +3,7 @@ import FeaturedProperties from "../../components/featuredProperties/FeaturedProp
 import Header from "../../components/header/Header";
 import Navbar from "../../components/navbar/Navbar";
 import PropertyList from "../../components/propertyList/PropertyList";
+import MailList from "../../components/mailList/MailList";
 import "./home.css";
 
 const Home = () => {
@@ -16,6 +17,7 @@ const Home = () => {
         <PropertyList />
         <h1 className="homeTitle">Homes guests love</h1>
         <FeaturedProperties />
+        <MailList />
       </div>
     </div>
   );
