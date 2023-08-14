73a1b208ed7477fe372c600119ad9590674e9cca
Bhupendra <shahi23.bhupendra11@gmail.com>
2023-08-14 13:40:00 +0530

    fixed bugs

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -2,7 +2,7 @@ export const exerciseOptions = {
   method: 'GET',
   headers: {
     'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
-    'X-RapidAPI-Key': 'e8253d780emsh04648c377277f96p1fb1ccjsnd83c1c00f82e',
+    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
   },
 };
 
@@ -10,7 +10,7 @@ export const youtubeOptions = {
   method: 'GET',
   headers: {
     'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
-    'X-RapidAPI-Key': 'e8253d780emsh04648c377277f96p1fb1ccjsnd83c1c00f82e',
+    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
   },
 };
 
